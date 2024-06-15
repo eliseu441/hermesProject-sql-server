6be27606b313b99b9d3cafeda168c7cbb9ba1a29
eliseu441 <eliseu441@gmail.com>
2024-06-15 20:07:47 -0300

    m

--- front/src/services/Api.js
+++ front/src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = `${import.meta.env.REACT_APP_BASE_URL}/api`
+const baseURL = `/api`
 //const baseURL = `http://localhost:3001/api`
 
 export default axios.create({
