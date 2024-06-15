e229ef3dbc769434b054acc550252c0ad94d427b
eliseu441 <eliseu441@gmail.com>
2024-06-15 19:28:24 -0300

    nova hp

--- front/src/services/Api.js
+++ front/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const baseURL = `${process.env.REACT_APP_BASE_URL}/api`
-const baseURL = `http://localhost:3001/api`
+const baseURL = `${import.meta.env.REACT_APP_BASE_URL}/api`
+//const baseURL = `http://localhost:3001/api`
 
 export default axios.create({
 	baseURL,
